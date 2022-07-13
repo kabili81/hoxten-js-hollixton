f0d37988d3881031cc10e69a9eea2f172128d080
Kabil Ibrahimi <kabili81@hotmail.com>
2022-07-13 15:24:31 +0200

    update

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import './style.css'
+
+let state = {
+
+}
+
+
+function render () {
+//   <main>
+//   <div>
+//     <img src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1" alt="" width="200" />
+//     <h3>Crewneck T-Shirt 3-Pack</h3>
+//     <h3>40</h3>
+//     <h3>21.99</h3>
+//   </div>
+// </main>
+let mainEl = document.createElement('main')
+
+let divMain = document.createElement('div')
+let imgDiv = document.createElement('img')
+let nameH3Div = document.createElement('h3')
+let priceH3Div = document.createElement('h3')
+let discountedPriceH3Div = document.createElement('h3')
+
+}
